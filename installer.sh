75e1a5211981524b5b48dfae4f0fd1d3409f4a0e
Ziro41 <cifernandez@frba.utn.edu.ar>
2024-03-01 20:58:16 +0000

    wait

--- installer.sh
+++ installer.sh
@@ -11,13 +11,13 @@ then
 fi	
 
 fdisk $disk
-
+wait
 mkfs.ext4 /dev/root
 mkfs.fat -F 32 /dev/efi
-
+wait
 mount /dev/root /mnt
 mount --mkdir /dev/efi /mnt/boot
-
+wait
 pacstrap -K /mnt base linux linux-firmware vim grub 
 
 gensfstab -U /mnt >> /mnt/etc/fstab
