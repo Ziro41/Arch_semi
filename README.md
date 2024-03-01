fe0b3426bea2b16dea7d8aa9f4f2f03b318a43a8
Ziro41 <cifernandez@frba.utn.edu.ar>
2024-03-01 20:37:24 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Arch_semi_automatic_installation
---
+++ installer.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+
+
+echo "Enter the name of the disk"
+read disk
+if  [[ ! $(fdisk -l |  grep -q $disk) ]]; 
+then 
+	echo "The disk couldn't be found"
+	exit 1;
+fi	
+
+fdisk $disk
+
+mkfs.ext4 /dev/root
+mkfs.fat -F 32 /dev/efi
+
+mount /dev/root /mnt
+mount --mkdir /dev/efi /mnt/boot
+
+pacstrap -K /mnt base linux linux-firmware vim grub 
+
+gensfstab -U /mnt >> /mnt/etc/fstab
+ 
