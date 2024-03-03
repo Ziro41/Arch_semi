fda4bb492a30fd9ce6c2738f77f12c240ab646b4
Ziro41 <cifernandez@frba.utn.edu.ar>
2024-03-03 23:57:17 +0000

    Some fixes and more commands after chroot

--- installer.sh
+++ installer.sh
@@ -12,27 +12,34 @@ fi
 
 fdisk $disk
 wait
-mkfs.ext4 ($disk)2
-mkfs.fat -F 32 ($disk)1
+mkfs.ext4 $(disk)2
+mkfs.fat -F 32 $(disk)1
 wait
-mount ($disk)2 /mnt
-mount --mkdir ($disk)1 /mnt/boot
+mount $(disk)2 /mnt
+mount --mkdir $(disk)1 /mnt/boot
 wait
-pacstrap -K /mnt base base-devel linux linux-firmware linux-header pipewire  mesa vim grub mtools sudo openssh networkmanager efibootmgr bluez bluez-utils xf86-video-intel xorg lightdm lightdm-gtk-greeter
-genfstab -U /mnt >> /mnt/etc/fstab
-
+pacstrap -K /mnt base base-devel linux linux-firmware  
+vim genfstab -U /mnt >> /mnt/etc/fstab
 chroot /mnt bin/bash << "EOF"
 ln -sf /usr/share/America/Argentina/Buenos_Aires /etc/localtime
 hwclock --systohc
 vim /etc/locale.gen
 wait
 locale-gen
-echo "LANG=es_UTF-8" > /etc/locale.conf
-echo "KEYMAP=es" > /etc/vconsole.conf
-echo "Zarch" >/etc/hostname
+pacman -Syu
+pacman -S linux-headers pipewire mesa grub mtools sudo openssh networkmanager efibootmgr bluez bluez-utils xf86-video-intel xorg lightdm lightdm-gtk-greeter 
+echo "LANG=es_UTF-8" >> /etc/locale.conf
+echo "KEYMAP=es" >> /etc/vconsole.conf
+echo "Zarch" >> /etc/hostname
+echo "127.0.0.1\tlocalhost\n::1\tlocalhost\n127.0.1.1\tZarch.localdomain\tZarch" >> /etc/hosts
 passwd
 wait
 useradd -m -f users -G ziro
 passwd ziro
 wait
- 
+EDITOR=vim visudo
+wait
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+systemctl enable NetworkManager
+systemctl enable bluetooth 
