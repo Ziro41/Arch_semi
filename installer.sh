c9cf8a2636ecdd2d02c74cb0c3ddedc6b1d0afe3
Ziro41 <cifernandez@frba.utn.edu.ar>
2024-03-04 12:07:45 +0000

    fixes

--- installer.sh
+++ installer.sh
@@ -4,7 +4,7 @@
 
 echo "Enter the name of the disk"
 read disk
-if  [[  $(fdisk -l |  grep  $disk) ]]; 
+if   $(fdisk -l |  grep  $disk); 
 then 
 	echo "The disk couldn't be found"
 	exit 1;
@@ -12,14 +12,14 @@ fi
 
 fdisk $disk
 wait
-mkfs.ext4 $(disk)2
-mkfs.fat -F 32 $(disk)1
+mkfs.ext4 "$(disk)2"
+mkfs.fat -F 32 "$(disk)1"
 wait
-mount $(disk)2 /mnt
-mount --mkdir $(disk)1 /mnt/boot
+mount "$(disk)2" /mnt
+mount --mkdir "$(disk)1" /mnt/boot
 wait
-pacstrap -K /mnt base base-devel linux linux-firmware  
-vim genfstab -U /mnt >> /mnt/etc/fstab
+pacstrap -K /mnt base base-devel linux linux-firmware vim
+genfstab -U /mnt >> /mnt/etc/fstab
 chroot /mnt bin/bash << "EOF"
 ln -sf /usr/share/America/Argentina/Buenos_Aires /etc/localtime
 hwclock --systohc
@@ -42,4 +42,5 @@ wait
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 systemctl enable NetworkManager
-systemctl enable bluetooth 
+systemctl enable bluetooth
+EOF 
